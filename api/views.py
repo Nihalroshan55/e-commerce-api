f03656ca44f45d797871c95c5fe58021236fdba7
Nihal <nihalroshan55@gmail.com>
2023-12-12 21:10:11 +0530

    added ProductViewSet and tested

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Customer
-from api.serializer import CustomerSerializer
+from api.models import Customer,Product
+from api.serializer import CustomerSerializer,ProductSerializer
 
 # Create your views here.
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
