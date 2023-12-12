db93911559d2008b3253e336fe642920b44e18be
Nihal <nihalroshan55@gmail.com>
2023-12-12 19:32:57 +0530

    productserializer added

--- api/serializer.py
+++ api/serializer.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from api.models import Customer
+from api.models import Customer,Product
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'contact_number', 'email']
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'weight']
         extra_kwargs = {
             'name': {'validators': []},
         }
