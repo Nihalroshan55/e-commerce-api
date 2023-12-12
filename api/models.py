149baea67d44159da24ce09d5b63535eedc1be1d
Nihal <nihalroshan55@gmail.com>
2023-12-12 19:12:20 +0530

    model customer and product added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
