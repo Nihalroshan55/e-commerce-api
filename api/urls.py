af6c36ff64ae9197eac9d17bcf453e38830e715e
Nihal <nihalroshan55@gmail.com>
2023-12-12 20:44:02 +0530

    added and tested ProductViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from api.views import CustomerViewSet,ProductViewSet
+from api.views import CustomerViewSet
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet, basename='customer')
-router.register(r'products', ProductViewSet, basename='product')
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,10 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
-from api.models import Customer, Product
-from api.serializer import CustomerSerializer , ProductSerializer
+from api.models import Customer
+from api.serializer import CustomerSerializer
 
 # Create your views here.
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
