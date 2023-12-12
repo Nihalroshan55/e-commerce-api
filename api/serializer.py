6eaafe166ce4dce743d03bf865d0fda02c893cd7
Nihal <nihalroshan55@gmail.com>
2023-12-12 20:02:56 +0530

    added orderitemserializer and orderserializer

--- api/serializer.py
+++ api/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from api.models import Customer,Product
-
+from api.models import Customer,Product,OrderItem
+from django.utils import timezone
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,4 +20,24 @@ class ProductSerializer(serializers.ModelSerializer):
     def validate_weight(self, value):
         if value <= 0 or value > 25:
             raise serializers.ValidationError("Weight must be a positive decimal and not more than 25kg.")
+        return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'order', 'product', 'quantity']
+        
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'order_number', 'customer', 'order_date', 'address', 'order_items']
+        extra_kwargs = {
+            'order_number': {'read_only': True},
+        }
+    def validate_order_date(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError("Order date cannot be in the past.")
         return value
