77c000b0c22b645c92c87e06bbed3305dbaa6a1f
Nihal <nihalroshan55@gmail.com>
2023-12-12 20:16:18 +0530

    added CustomerViewSet and urls

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Customer
+from api.serializer import CustomerSerializer
 
 # Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
